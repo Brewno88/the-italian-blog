30fa2f8e3468718e12405c1c385b43a985b78720
Vincenzo <ilcontevince@gmail.com>
2020-04-20 19:35:32 +0100

    create posts pages and post page template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    {
+      allContentfulBlogPost {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
 
-// You can delete this file if you're not using it
+  if (result.errors) {
+    reporter.panic("Error loading lesson", JSON.stringify(result.errors))
+  }
+  result.data.allContentfulBlogPost.nodes.forEach(post => {
+    actions.createPage({
+      path: `/${post.slug}/`,
+      component: require.resolve("./src/templates/post-page.js"),
+      context: {
+        slug: post.slug,
+      },
+    })
+  })
+}
---
+++ src/templates/post-page.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styled from "styled-components"
+
+const PostPage = () => {
+  return <Wrap>PostPage</Wrap>
+}
+
+//* styled-component < 💅>
+const Wrap = styled.div``
+
+export default PostPage
