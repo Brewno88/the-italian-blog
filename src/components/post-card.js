3d3796e393eecca903aea1e4fe203f8608799ac5
Vincenzo <ilcontevince@gmail.com>
2020-04-21 17:13:01 +0100

    styled post-card

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import Img from "gatsby-image"
 
 import styled from "styled-components"
+import { colors } from "../utils/variables"
 
 const PostCard = ({
   id,
@@ -13,31 +14,91 @@ const PostCard = ({
   createdAt,
   thumbnail,
 }) => {
+  console.log(colors)
   return (
-    <Article>
-      <Img className="thumbnail" fluid={thumbnail.fluid} />
-      <h2>
-        <Link to={`/${slug}`}>{title}</Link>
-      </h2>
-      <small>{createdAt}</small>
-      <p>{description}</p>
-      <ul>
-        {tags.map((tag, index) => (
-          <li className="tag" key={index}>
-            {tag}
-          </li>
-        ))}
-      </ul>
-      <Link to={`/${slug}`}>Read More...</Link>
+    <Article className="card">
+      <Img
+        className="thumbnail"
+        fluid={thumbnail.fluid}
+        style={{ borderRadius: ".8rem .8rem 0 0" }}
+      />
+      <div className="information">
+        <h2 className="title">
+          <Link to={`/${slug}`}>{title}</Link>
+        </h2>
+        <small className="date">{createdAt}</small>
+        <p className="description">{description}</p>
+        <div className="bottom">
+          <div className="tags">
+            {tags.map((tag, index) => (
+              <h6 className="tag" key={index}>
+                {`#${tag}`}
+              </h6>
+            ))}
+          </div>
+          <Link className="read-more" to={`/${slug}`}>
+            <button className="btn">Read More...</button>
+          </Link>
+        </div>
+      </div>
     </Article>
   )
 }
 
 //* styled-component < 💅>
 const Article = styled.div`
-  height: 30rem;
+  margin-bottom: 2rem;
+  border: solid 3px ${colors.secondary};
+  border-radius: 1rem;
+  z-index: 2;
   .thumbnail {
-    height: 33%;
+    height: 15rem;
+  }
+  .information {
+    padding: 1rem 1rem 1rem 1rem;
+    .title {
+      font-size: 2rem;
+      margin-bottom: 0.5rem;
+      a {
+        color: ${colors.text};
+        text-decoration: none;
+      }
+    }
+    .date {
+      font-style: italic;
+      margin-bottom: 1rem;
+    }
+    .description {
+      margin-top: 0.5rem;
+    }
+    .bottom {
+      display: flex;
+      justify-content: space-between;
+      .tags {
+        display: flex;
+        .tag {
+          list-style: none;
+          font-size: 0.7rem;
+          margin: 0 0.2rem;
+          background: ${colors.primary};
+          color: ${colors.secondary};
+          border-radius: 3rem;
+          padding: 0.4rem;
+          cursor: pointer;
+          align-self: center;
+        }
+      }
+      .btn {
+        color: ${colors.background};
+        background: ${colors.secondary};
+        cursor: pointer;
+        border-radius: 6rem;
+        border: none;
+        padding: 0.2rem 0.3rem;
+        font-size: 0.7rem;
+        font-weight: bold;
+      }
+    }
   }
 `
 
