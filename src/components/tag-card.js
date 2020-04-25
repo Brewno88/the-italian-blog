b74653557ea6e7333c15de95ad53b53dd592f3a1
Vincenzo <ilcontevince@gmail.com>
2020-04-25 18:16:24 +0100

    create tag-card component for all-tags page (need to be fixed) - in each tag section there must be all the tag posts

--- src/components/tag-card.js
+++ src/components/tag-card.js
@@ -1,34 +1,45 @@
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import { Link, graphql, StaticQuery } from "gatsby"
 import styled from "styled-components"
+import Img from "gatsby-image"
 
-const TagCard = ({ data, tag, children }) => {
+const TagCard = ({ id, title, tag, slug, description, thumbnail }) => {
   return (
-    <StaticQuery
-      query={graphql`
-        {
-          allContentfulBlogPost(filter: { tags: { eq: "vegetarian" } }) {
-            nodes {
-              tags
-              thumbnail {
-                fluid {
-                  srcWebp
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={data => (
-        <Wrap>
-          <h2>{children}</h2>
-        </Wrap>
-      )}
-    />
+    <Wrap>
+      <Link to={`/${slug}`}>
+        <Img
+          className="thumbnail"
+          fluid={thumbnail.fluid}
+          style={{
+            borderRadius: ".8rem",
+            width: "20rem",
+            height: "100%",
+          }}
+        />
+      </Link>
+      <div
+        style={{
+          width: "15rem",
+          background: "white",
+          borderRadius: "1rem",
+          position: "absolute",
+          bottom: "1rem",
+          textAlign: "center",
+        }}
+      >
+        <h2 style={{ margin: "auto", padding: "1rem" }}>{title}</h2>
+      </div>
+    </Wrap>
   )
 }
 
 //* styled-component < 💅>
-const Wrap = styled.div``
+const Wrap = styled.div`
+  position: relative;
+  width: 20rem;
+  display: inline-flex;
+  justify-content: center;
+  height: 10rem;
+`
 
 export default TagCard
--- src/templates/all-tags-template.js
+++ src/templates/all-tags-template.js
@@ -1,32 +1,61 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+
 import TagCard from "../components/tag-card"
+import { colors } from "../utils/variables"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export const getTags = graphql`
   {
-    tags: allContentfulBlogPost {
+    allContentfulBlogPost {
       nodes {
         tags
+        thumbnail {
+          fluid {
+            ...GatsbyContentfulFluid_withWebp
+          }
+        }
+        title
+        description
       }
     }
   }
 `
 
 const SecondPage = ({ data, location, pageContext }) => {
-  const allPosts = data.tags.nodes
+  const allPosts = data.allContentfulBlogPost.nodes
   const { uniqueTags, tagCounts } = pageContext
 
   return (
     <Layout location={location}>
       <SEO title="Tags" />
-      {uniqueTags.map((tag, index) => (
-        <TagCard key={index} tag={tag}>
-          {tag}
-        </TagCard>
-      ))}
+      {uniqueTags.map((tag, index) => {
+        return (
+          <div
+            style={{
+              background: index % 2 === 0 ? colors.primary : colors.background,
+              height: "20rem",
+              marginBottom: "2rem",
+              padding: "1rem 0",
+            }}
+            key={index}
+          >
+            <Link
+              to={`/tags/${tag}`}
+              style={{
+                textDecoration: "none",
+                color: colors.text,
+              }}
+            >
+              <h2>
+                {tag}: {tagCounts[tag]}
+              </h2>
+            </Link>
+          </div>
+        )
+      })}
     </Layout>
   )
 }
