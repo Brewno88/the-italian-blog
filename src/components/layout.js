dabc2c39eb78fe3e8b2a59231d0f1da179929207
Vincenzo <ilcontevince@gmail.com>
2020-04-29 12:29:31 +0100

    add GlobalStyle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { createGlobalStyle } from "styled-components"
 
 import Header from "./header"
 import Footer from "./footer"
-import "./global.css"
-import { colors } from "../utils/variables"
+// import "./global.css"
+import { colors, typography, appearance, animation } from "../utils/variables"
 import ScrollToTop from "./ScrollTopButton"
 
 const Layout = ({ children, location, posts }) => {
@@ -20,6 +21,7 @@ const Layout = ({ children, location, posts }) => {
 
   return (
     <>
+      <GlobalStyle />
       <Header
         siteTitle={data.site.siteMetadata.title}
         location={location}
@@ -30,7 +32,7 @@ const Layout = ({ children, location, posts }) => {
         style={{
           background: colors.ternary,
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: appearance.headerWidth,
           padding: `${
             location.pathname === "/" || location.pathname === "/tags"
               ? "6rem 1rem"
@@ -38,12 +40,41 @@ const Layout = ({ children, location, posts }) => {
           }`,
         }}
       >
-        {children}
         <ScrollToTop />
+        {children}
       </main>
       <Footer siteTitle={data.site.siteMetadata.title} />
     </>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+html{
+  font-family: sans-serif;
+}
+body{
+  background: ${colors.ternary};
+  font-size: 62.5%;
+}
+a{
+  font-weight: bold;
+  color: ${colors.secondary};
+  text-shadow: ${typography.shadowPrimary};
+  transition: ${animation.transition.hover};
+
+  &:hover{
+    color: ${colors.primary};
+    text-shadow: ${typography.shadowSecondary};
+    text-decoration: none;
+    transition: ${animation.transition.hover};
+  }
+}
+h1, h2,h3,h4,h5,h6{
+  font-weight: bold;
+  color: ${colors.text};
+  text-shadow: ${typography.shadowPrimary};
+
+}
+`
 
 export default Layout
