3a2b9a36df0749f23f5afafb62f47eb477478455
Vincenzo <ilcontevince@gmail.com>
2020-04-29 11:48:54 +0100

    add variables

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,7 +1,28 @@
 export const colors = {
   primary: "#B0EDE8",
-  secondary: "#707070",
+  secondary: "#282938",
   ternary: "#FBFBF2",
   text: "#1D1D1D",
   background: "#FFFFFF",
+}
+
+export const typography = {
+  shadowPrimary: `.1rem .1rem 0 ${colors.primary}`,
+  shadowSecondary: `.1rem .1rem 0 ${colors.secondary}`,
+  shadowTernary: `.1rem .1rem 0 ${colors.ternary}`,
+}
+
+export const appearance = {
+  articleWidth: 720,
+  headerWidth: 960,
+}
+
+export const keyframe = {
+  hover: "from{ } to {}",
+}
+
+export const animation = {
+  transition: {
+    hover: "all .3s linear",
+  },
 }
